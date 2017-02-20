667898528ea2acca7934606bb01964bccea35abf
Ksenia D <Ksenia D>
2017-02-20 14:12:37 -0500

    initial files

---
+++ node.js
@@ -0,0 +1,76 @@
+var inquirer = require("inquirer");
+var fs = require('fs');
+
+function BasicCard(front, back) {
+    this.front = front;
+    this.back = back;
+};
+
+function ClozeCard(fullText, clozeText) {
+    this.full = fullText;
+    this.cloze = clozeText;
+    this.partText = fullText.replace(this.cloze, "...");
+};
+// ClozeCard.partText = ClozeCard.full.replace(clozeText, "...");
+
+function writeCard(newCard) {
+		
+         fs.appendFile('./cards.txt', JSON.stringify(newCard) +", ", function(err) {
+		  // If an error was experienced we say it.
+		  if (err) {
+		    console.log(err);
+		  }
+		  // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+		  else {
+		    console.log("card Added!");
+		  }
+		});
+	 
+}
+
+
+inquirer.prompt([{
+        type: "rawlist",
+        message: "What would you like to do today?",
+        choices: ["Create Basic Flashcard", "Create Cloze Flashcard"],
+        default: ["Create Basic Flashcard"],
+        name: "commands"
+
+    }]).then(function(user) {
+    	if (user.commands === "Create Basic Flashcard") {
+                    inquirer.prompt([{
+	                type: "input",
+	                message: "Create Front",
+	                name: "basic_front"
+	            }, 
+	            {
+	                type: "input",
+	                message: "Create Back",
+	                name: "basic_back"
+	            
+	            }]).then(function(basicCard) {
+	                var basicCard = new BasicCard(basicCard.basic_front, basicCard.basic_back);
+	                console.log(basicCard.front, basicCard.back);
+	                //write in text file
+	                writeCard(basicCard);
+	            });    
+           } else {
+           		 inquirer.prompt([{
+	                type: "input",
+	                message: "Write Full Text",
+	                name: "full_text"
+	            }, 
+	            {
+	                type: "input",
+	                message: "What is the Cloze-deleted Portion of the Text",
+	                name: "cloze_text"
+	            
+	            }]).then(function(clozeCard) {
+	            	var clozeCard = new ClozeCard(clozeCard.full, clozeCard.cloze, clozeCard.partText);
+	                console.log(clozeCard);
+	                //write in text file
+	                writeCard(clozeCard);
+	            });    
+           }
+    	});
+
